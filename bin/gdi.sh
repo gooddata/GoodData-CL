9127e0be69dbfcb7484c2599ab4b14b5ea9fe3dd
Jakub Nesetril <jakub@gooddata.com>
2010-06-04 10:59:49 +0200

    Fixed project directory resolution

--- bin/gdi.sh
+++ bin/gdi.sh
@@ -4,7 +4,7 @@
 
 file=`readlink $0`;      # resolve symlinks if necessary
 file=${file:-$0};        # if not a symlink, use the $0
-PRJ_BIN=`dirname\`dirname $file\``  # base directory
+PRJ_BIN=`dirname \`dirname $file\``  # base directory
 
 . $PRJ_BIN/bin/cfg.sh
 
