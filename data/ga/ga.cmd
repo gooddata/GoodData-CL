7d99d6541112c733d7f674b9a88e2df7424987bf
zdenek <zd@gooddata.com>
2010-06-06 01:39:23 +0200

    Added Google Analytics Connector

--- data/ga/ga.cmd
+++ data/ga/ga.cmd
@@ -1,6 +1,6 @@
 CreateProject(name=GA);
 GenerateGoogleAnalyticsConfigTemplate(name=GA,configFile=data/ga/ga.config.xml,dimensions=ga:date|ga:browser,metrics=ga:visits|ga:pageviews);
-LoadGoogleAnalytics(configFile=data/ga/ga.config.xml,username=user@gmail.com,password=password,profileId=ga:7468896,dimensions=ga:date|ga:browser,metrics=ga:visits|ga:pageviews,startDate=2010-01-01,endDate=2010-05-31);
+LoadGoogleAnalytics(configFile=data/ga/ga.config.xml,username=user@gmail.com,password=password,profileId=ga:7467765,dimensions=ga:date|ga:browser,metrics=ga:visits|ga:pageviews,startDate=2010-01-01,endDate=2010-05-31);
 GenerateMaql(maqlFile=data/ga/ga.maql);
 ExecuteMaql(maqlFile=data/ga/ga.maql);
 TransferLastSnapshot(incremental=true);
