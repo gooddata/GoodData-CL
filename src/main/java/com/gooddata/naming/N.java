912f542303251805b5bae0cbb31c1ab5d6c4aaf7
Zdenek Svoboda <zdenek@zdminimac.local>
2010-06-10 20:23:00 +0200

    Naming

---
+++ src/main/java/com/gooddata/naming/N.java
@@ -0,0 +1,25 @@
+package com.gooddata.naming;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zdenek
+ * Date: Jun 10, 2010
+ * Time: 11:56:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class N {
+
+    // table generator constants
+    public final static String SRC_PFX = "o_";
+    public final static String LKP_PFX = "d_";
+    public final static String NM_PFX = "nm_";
+    public final static String FCT_PFX = "f_";
+    public final static String DT = "dt";
+    public final static String DT_PFX = DT+"_";
+    public final static String ID = "id";
+    public final static String HSH = "hashid";
+    public final static String SRC_ID = SRC_PFX+"_"+ID;
+
+
+    public static final String FACTS_OF = "factsof";
+}
