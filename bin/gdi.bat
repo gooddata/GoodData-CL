9109a28f00bb5766af55eaac0a192bc1f996d721
zdenek <zd@gooddata.com>
2010-06-14 23:30:27 +0200

    Modified gdi.bat

--- bin/gdi.bat
+++ bin/gdi.bat
@@ -1,2 +1,3 @@
-PRJ_BIN=%~dp0 
-%JAVA_HOME%/bin/java.exe  -Dlog4j.configuration=%PRJ_BIN%/../log4j.configuration -Dderby.system.home=%PRJ_BIN%/../db -Djava.io.tmpdir=%PRJ_BIN%/../tmp -classpath %PRJ_BIN%/..;%PRJ_BIN%/../target/GoodDataDI-0.6-jar-with-dependencies.jar com.gooddata.processor.GdcDI %*
+SET PRJ_BIN=%~dp0
+echo %PRJ_BIN%
+%JAVA_HOME%\bin\java.exe  -Dlog4j.configuration=%PRJ_BIN%\..\log4j.configuration -Dderby.system.home=%PRJ_BIN%\..\db -Djava.io.tmpdir=%PRJ_BIN%/../tmp -classpath %PRJ_BIN%\..;%PRJ_BIN%\..\target\GoodDataDI-0.6-jar-with-dependencies.jar com.gooddata.processor.GdcDI %*
