ec1f7c7cc3ae5bcd14f3f5ee0ab87f9382c86a2e
zdenek <zd@gooddata.com>
2010-06-23 14:03:17 +0200

    Splitted README.md to the source and binary parts. Fixed the bug in the gdi.sh.

--- cli-distro/bin/gdi.sh
+++ cli-distro/bin/gdi.sh
@@ -7,14 +7,6 @@ A=${A:-$0}      # if original wasn't a symlink, BSD returns empty string
 SCRIPT_REAL_PATH=$(cd ${A%/*} && echo $PWD/${A##*/}) # path to original shell script, resolved from relative paths
 PROJECT_DIR=`dirname \`dirname $SCRIPT_REAL_PATH\``  # base directory
 
-QUOTED_ARGS=""
-while [ "$1" != "" ] ; do
-
-  QUOTED_ARGS="$QUOTED_ARGS \"$1\""
-  shift
-
-done
-
 # OS specific support.  $var _must_ be set to either true or false.
 cygwin=false;
 darwin=false;
@@ -94,4 +86,4 @@ done
 
 TMPDIR=/tmp
 
-"$JAVACMD" -Xmx512M -Dlog4j.configuration=$PROJECT_DIR/log4j.configuration -Dderby.system.home=$PROJECT_DIR/db -Djava.io.tmpdir=$TMPDIR -cp ${CLSPTH} com.gooddata.processor.GdcDI $QUOTED_ARGS
+"$JAVACMD" -Xmx512M -Dlog4j.configuration=$PROJECT_DIR/log4j.configuration -Dderby.system.home=$PROJECT_DIR/db -Djava.io.tmpdir=$TMPDIR -cp ${CLSPTH} com.gooddata.processor.GdcDI $*
