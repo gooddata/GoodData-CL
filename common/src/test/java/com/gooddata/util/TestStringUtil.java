6e71c0be37e68ef6a922b4072a11dfb37aa311a2
zdenek <zd@gooddata.com>
2010-07-14 21:56:21 +0200

    Added FileUtil tests

---
+++ common/src/test/java/com/gooddata/util/TestStringUtil.java
@@ -0,0 +1,62 @@
+/*
+ * /*
+ *  * Copyright (c) 2009 GoodData Corporation.  All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions
+ *  * are met:
+ *  * 1. Redistributions of source code must retain the above copyright
+ *  *    notice, this list of conditions and the following disclaimer.
+ *  * 2. Redistributions in binary form must reproduce the above copyright
+ *  *    notice, this list of conditions and the following disclaimer in the
+ *  *    documentation and/or other materials provided with the distribution.
+ *  * 3. Redistributions in any form must be accompanied by information on
+ *  *    how to obtain complete source code for this software and any
+ *  *    accompanying software that uses this software.  The source code
+ *  *    must either be included in the distribution or be available for no
+ *  *    more than the cost of distribution plus a nominal fee, and must be
+ *  *    freely redistributable under reasonable conditions.  For an
+ *  *    executable file, complete source code means the source code for all
+ *  *    modules it contains.  It does not include source code for modules or
+ *  *    files that typically accompany the major components of the operating
+ *  *    system on which the executable file runs.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY GOODDATA ``AS IS'' AND ANY EXPRESS OR
+ *  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ *  * NON-INFRINGEMENT, ARE DISCLAIMED.  IN NO EVENT SHALL ORACLE BE LIABLE
+ *  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *  * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  */
+
+package com.gooddata.util;
+
+import junit.framework.TestCase;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * GoodData
+ *
+ * @author zd <zd@gooddata.com>
+ * @version 1.0
+ */
+public class TestStringUtil extends TestCase {
+
+    private static Logger l = Logger.getLogger(TestStringUtil.class);
+
+    public void testformatShortName() throws Exception {
+        BufferedReader in =  FileUtil.createBufferedUtf8Reader(getClass().getResourceAsStream("/com/gooddata/util/test1.txt"));
+        assertEquals(StringUtil.formatShortName(in.readLine()),"aiiarsiarsiaeaeieaeeareisariseaiarsreuireiurieufdnfmdfdnm323232322");
+        assertEquals(StringUtil.formatShortName(in.readLine()),"siarsiaeaeieaedffg");
+    }
+
+
+}
