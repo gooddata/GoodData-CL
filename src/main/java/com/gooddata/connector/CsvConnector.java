78b68aaa3280b7925d14b68f9762b6dc20b751ae
Pavel Kolesnikov <pavel@gooddata.com>
2010-07-21 17:46:55 +0200

    BUGFIX: load existing column names already preformatted to prevent duplicities caused by duplicate fields with extra whitespace etc

--- src/main/java/com/gooddata/connector/CsvConnector.java
+++ src/main/java/com/gooddata/connector/CsvConnector.java
@@ -239,7 +239,7 @@ public class CsvConnector extends AbstractConnector implements Connector {
     private static Set<String> getColumnNames(List<SourceColumn> columns) {
     	Set<String> result = new HashSet<String>();
 		for (final SourceColumn col : columns) {
-			result.add(col.getName());
+			result.add(StringUtil.formatShortName(col.getName()));
 		}
 		return result;
 	}
