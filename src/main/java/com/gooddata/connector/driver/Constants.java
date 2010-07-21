618d56c47c8d102e701027d23c9e61f7c4c39587
Pavel Kolesnikov <pavel@gooddata.com>
2010-07-21 21:28:10 +0200

    Reduced table length limit to workaround creating temporary suffixed tables on the GD server

--- src/main/java/com/gooddata/connector/driver/Constants.java
+++ src/main/java/com/gooddata/connector/driver/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	// TODO native2ascii
 	public final static String[] DISCARD_CHARS = {"$", "%", ",", "(", ")", "Û", "£", "´"};
 	
-	public final static int MAX_TABLE_NAME_LENGTH = 44;
+	public final static int MAX_TABLE_NAME_LENGTH = 36;
 
 }
