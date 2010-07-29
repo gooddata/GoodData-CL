46a760a57d28644e013b423b7835fad64b7d2ed8
zdenek <zd@gooddata.com>
2010-07-29 15:30:08 +0200

    Added the empty dates test

--- backend/src/test/java/com/gooddata/derby/extension/TestDerbyExtensions.java
+++ backend/src/test/java/com/gooddata/derby/extension/TestDerbyExtensions.java
@@ -28,8 +28,8 @@ import junit.framework.TestCase;
 public class TestDerbyExtensions extends TestCase {
 
     public void testdttoi() {
-        assertEquals(DerbyExtensions.dttoi("","yyyy-MM-dd"),2147483647);
-        assertEquals(DerbyExtensions.dttoi("AA","yyyy-MM-dd"),2147483647);
+        assertEquals(DerbyExtensions.dttoi("","yyyy-MM-dd"),0);
+        assertEquals(DerbyExtensions.dttoi("AA","yyyy-MM-dd"),0);
         assertEquals(DerbyExtensions.dttoi("2010-06-15","yyyy-MM-dd"),40343);
     }
 }
