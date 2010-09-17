c98f8d90f72d67e53ed9eae73a991af2fd146df3
Pavel Kolesnikov <pavel@gooddata.com>
2010-09-17 14:05:29 +0200

    BUGFIX: 67 - fixed "odd quote character" error if the 4096th character is an escaped double-quote + test added

--- common/src/test/java/com/gooddata/util/CSVReaderTest.java
+++ common/src/test/java/com/gooddata/util/CSVReaderTest.java
@@ -40,12 +40,32 @@ public class CSVReaderTest {
 		CSVReader csv = new CSVReader(reader);
 
 		final int expectedRows = 2;
+		final int expectedCols = 4;
+
 		int i = 0;
 		String[] line = null;
 		while ((line = csv.readNext()) != null) {
-			Assert.assertEquals(4, line.length);
+			Assert.assertEquals(expectedCols, line.length);
 			i++;
 		}
 		Assert.assertEquals(expectedRows, i);
 	}
+    
+    @Test
+	public void testChunkBoundaryEscape() throws IOException {
+    	Reader reader = new InputStreamReader(
+				CSVReaderTest.class.getResourceAsStream("/com/gooddata/util/chunk-boundary.csv"),
+				"utf8");
+		CSVReader csv = new CSVReader(reader);
+
+		final int expectedRows = 58;
+		final int expectedCols = 13;
+		int i = 0;
+		String[] line = null;
+		while ((line = csv.readNext()) != null) {
+			Assert.assertEquals(expectedCols, line.length);
+			i++;
+		}
+		Assert.assertEquals(expectedRows, i);
+    }
 }
