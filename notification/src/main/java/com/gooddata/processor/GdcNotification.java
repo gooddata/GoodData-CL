de997258869f0f98ce31bd58ef4b70fc7cd985be
ZD <zd@gooddata.com>
2010-11-18 12:07:22 +0100

    1.2.5-SNAPSHOT

--- notification/src/main/java/com/gooddata/processor/GdcNotification.java
+++ notification/src/main/java/com/gooddata/processor/GdcNotification.java
@@ -373,7 +373,7 @@ public class GdcNotification {
         }
 
         if(cp.containsKey(CLI_PARAM_VERSION[0])) {
-            l.info("GoodData Notification Tool version 1.2.4-SNAPSHOT" +
+            l.info("GoodData Notification Tool version 1.2.5-SNAPSHOT" +
                     ((BUILD_NUMBER.length()>0) ? ", build "+BUILD_NUMBER : "."));
             System.exit(0);
 
