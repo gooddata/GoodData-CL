1384cca4c762dd49d591d0162fb4d9aaf9199fdc
Pavel Kolesnikov <pavel@gooddata.com>
2010-11-29 09:42:11 -0800

    Fixed version number for user agent string and the version cmdline option (1.1.11-snapshot)

--- backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
+++ backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
@@ -1234,7 +1234,7 @@ public class GdcRESTApiWrapper {
     private static <T extends HttpMethod> T configureHttpMethod(T request) {
         request.setRequestHeader("Content-Type", "application/json");
         request.setRequestHeader("Accept", "application/json");
-        request.setRequestHeader("User-Agent", "GoodData CL/1.1.10-SNAPSHOT");
+        request.setRequestHeader("User-Agent", "GoodData CL/1.1.11-SNAPSHOT");
     	return request;
     }
 
