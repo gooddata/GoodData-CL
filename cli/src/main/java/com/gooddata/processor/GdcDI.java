4a6fc605d45a6201e826a3be85b657f4a7007cff
Pavel Kolesnikov <pavel@gooddata.com>
2010-12-04 16:03:54 -0800

    1.2.9 SNAPSHOT

--- cli/src/main/java/com/gooddata/processor/GdcDI.java
+++ cli/src/main/java/com/gooddata/processor/GdcDI.java
@@ -342,7 +342,7 @@ public class GdcDI implements Executor {
         }
 
         if(cp.containsKey(CLI_PARAM_VERSION[0])) {
-            l.info("GoodData CL version 1.2.8-SNAPSHOT" +
+            l.info("GoodData CL version 1.2.9-SNAPSHOT" +
                     ((BUILD_NUMBER.length()>0) ? ", build "+BUILD_NUMBER : "."));
             System.exit(0);
 
