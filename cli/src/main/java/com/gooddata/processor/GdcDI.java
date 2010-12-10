e1182e2c7eb79d7fc31894e5a5ac7d6b1e3cb519
ZD <zd@gooddata.com>
2010-12-10 13:22:27 +0100

    1.2.13-beta

--- cli/src/main/java/com/gooddata/processor/GdcDI.java
+++ cli/src/main/java/com/gooddata/processor/GdcDI.java
@@ -346,7 +346,7 @@ public class GdcDI implements Executor {
 
         if(cp.containsKey(CLI_PARAM_VERSION[0])) {
 
-            l.info("GoodData CL version 1.2.13-SNAPSHOT" +
+            l.info("GoodData CL version 1.2.13-BETA" +
                     ((BUILD_NUMBER.length()>0) ? ", build "+BUILD_NUMBER : "."));
             System.exit(0);
 
