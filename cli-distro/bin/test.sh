49d8b9d851a2101ad8540347c5cd53fa05dac23d
Pavel Kolesnikov <pavel@gooddata.com>
2011-01-27 13:39:06 +0100

    BUGFIX: fixed mistakenly damaget test script

--- cli-distro/bin/test.sh
+++ cli-distro/bin/test.sh
@@ -13,6 +13,95 @@ function drop() {
     bin/gdi.sh --backend "$backend" "$tmp"
 }
 
+for backend in MYSQL DERBY ; do
+    echo 'Running quotes demo'
+    bin/gdi.sh --backend "$backend" examples/quotes/quotes.txt
+
+    echo 'Updating the data set in the quotes demo'
+    echo "RetrieveProject(fileName = \"examples/quotes/pid\");" > "$tmp"
+    grep '^\(LoadCsv\|Transfer\)' examples/quotes/quotes.txt >> "$tmp"
+    bin/gdi.sh --backend "$backend" "$tmp"
+
+    echo 'Altering the server-side model (-2 attributes -1 fact +1 fact)'
+    bin/gdi.sh --backend "$backend" examples/quotes/alter.txt
+
+    echo 'Dropping the quotes project and snapshots'
+    drop "`cat examples/quotes/pid`"
+
+    echo 'Running the HR demo'
+    bin/gdi.sh --backend "$backend" examples/hr/1-department.txt
+    bin/gdi.sh --backend "$backend" examples/hr/2-employee.txt
+    bin/gdi.sh --backend "$backend" examples/hr/3-salary.txt
+
+    echo 'Updating the employee dataset'
+    echo "RetrieveProject(fileName = \"examples/hr/pid\");" > "$tmp"
+    grep '^\(LoadCsv\|Transfer\)' examples/hr/2-employee.txt >> "$tmp"
+    bin/gdi.sh --backend "$backend" "$tmp"
+
+    echo 'Dropping the HR project and snapshots'
+    drop "`cat examples/hr/pid`"
+
+    echo 'Running sfdc demo'
+    bin/gdi.sh --backend "$backend" examples/sfdc/sfdc.txt
+
+    echo 'Dropping the sfdc project and snapshots'
+    drop "`cat examples/sfdc/pid`"
+
+    echo 'Running jdbc demo'
+    bin/gdi.sh --backend "$backend" examples/jdbc/fundamentals.txt
+
+    echo 'Dropping the jdbc project and snapshots'
+    drop "`cat examples/jdbc/pid`"
+
+    echo 'Running ga demo'
+    bin/gdi.sh --backend "$backend" examples/ga/ga.txt
+
+    echo 'Dropping the ga project and snapshots'
+    drop "`cat examples/ga/pid`"
+
+    echo 'Running naming test'
+    bin/gdi.sh --backend "$backend" tests/naming/cmd.txt
+
+    echo 'Dropping the naming test project and snapshots'
+    drop "`cat tests/naming/pid`"
+
+    echo 'Running empty_lines test'
+    bin/gdi.sh --backend "$backend" tests/empty_lines/cmd.txt
+
+    echo 'Dropping the empty_lines test project and snapshots'
+    drop "`cat tests/empty_lines/pid`"
+
+    echo 'Running empty_dates test'
+    bin/gdi.sh --backend "$backend" tests/empty_dates/cmd.txt
+
+    echo 'Dropping the empty_dates test project and snapshots'
+    drop "`cat tests/empty_dates/pid`"
+
+    echo 'Running separators test'
+    bin/gdi.sh --backend "$backend" tests/separators/cmd.txt
+
+    echo 'Dropping the separators test project and snapshots'
+    drop "`cat tests/separators/pid`"
+
+    echo 'Running escapes test'
+    bin/gdi.sh --backend "$backend" tests/escapes/cmd.txt
+
+    echo 'Dropping the escapes test project and snapshots'
+    drop "`cat tests/escapes/pid`"
+
+    echo 'Running drop_snapshots test'
+    bin/gdi.sh --backend "$backend" tests/drop_snapshots/cmd.txt
+
+    echo 'Dropping the drop_snapshots test project and snapshots'
+    drop "`cat tests/drop_snapshots/pid`"
+
+    echo 'Running changed_cp_label test'
+    bin/gdi.sh --backend "$backend" tests/changed_cp_label/cmd.txt
+
+    echo 'Dropping the changed_cp_label test project and snapshots'
+    drop "`cat tests/changed_cp_label/pid`"
+done
+
 echo 'Running generate_csv_config test'
 bin/gdi.sh tests/generate_csv_config/quotes.txt
 facts=`grep FACT tests/generate_csv_config/quotes.config.xml | wc -l | sed 's,^ *,,'`
@@ -20,4 +109,3 @@ if [ "x$facts" != "x5" ] ; then
     echo "generate_csv_config test produced '$facts' FACT fields, 5 expected"
     exit 1
 fi
-
