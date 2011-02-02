94edace15e2c8e95fe29122ef746d27d362eee98
ZD <zd@gooddata.com>
2011-02-02 15:29:40 +0100

    Modified the Facebook Servlet

--- web/src/main/java/com/gooddata/web/WebInterface.java
+++ web/src/main/java/com/gooddata/web/WebInterface.java
@@ -23,11 +23,15 @@
 
 package com.gooddata.web;
 
+import com.gooddata.connector.Constants;
 import com.gooddata.util.FileUtil;
 import com.google.gdata.util.common.util.Base64DecoderException;
 import net.sf.json.JSONObject;
 import com.google.gdata.util.common.util.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.io.*;
 import java.util.*;
@@ -45,31 +49,43 @@ public class WebInterface extends HttpServlet {
 
     static private final String CREATE_TEMPLATE = "/mnt/projects/facebook/cmd/create.data.model.txt";
     static private final String LOAD_TEMPLATE = "/mnt/projects/facebook/cmd/load.data.txt";
-
     static private final String IN_QUEUE = "/mnt/projects/facebook/in_queue";
-
-
     static private String logPath = "/tmp/fblog.log";
     static private FileWriter logger;
     static final String form = "<!DOCTYPE HTML SYSTEM><html><head><title>GoodData Data Synchronization</title></head>" +
             "<body><form method='POST' action=''><table border='0'>" +
             "<tr><td><b>GoodData Username:</b></td><td><input type='text' name='EMAIL' value='john.doe@acme.com'/></td></tr>" +
             "<tr><td><b>Insight Graph API URL:</b></td><td><input type='text' size='80' name='BASE_URL' value='https://graph.facebook.com/175593709144814/insights/page_views/day'/></td></tr>" +
-            "<tr><td><b>Start Date (YYYY-MM-DD):</b></td><td><input type='text' name='START_DATE' value='2011-01-01'/></td></tr>" +
-            "<tr><td><b>End Date (YYYY-MM-DD):</b></td><td><input type='text' name='END_DATE' value='2011-01-31'/></td></tr>" +
-            "<tr><td><b>Create GoodData Project:</b></td><td><input type='checkbox' name='CREATE_PROJECT_FLAG' checked='1'/></td></tr>" +
+            "<tr><td><b>Start Date (YYYY-MM-DD):</b></td><td><input type='text' name='START_DATE' value='%START_DATE_INITIAL_VALUE%'/></td></tr>" +
+            "<tr><td><b>End Date (YYYY-MM-DD):</b></td><td><input type='text' name='END_DATE' value='%END_DATE_INITIAL_VALUE%'/></td></tr>" +
+            "<tr><td><b>GoodData Project ID:</b></td><td><input type='text' name='GDC_PROJECT_HASH' value=''/></td></tr>" +
+            "<tr><td><b>Create New GoodData Project:</b></td><td><input type='checkbox' name='CREATE_PROJECT_FLAG' checked='1'/></td></tr>" +
             "<tr><td colspan='2'>" +
             "<input type='hidden' name='TOKEN' value='%TOKEN%'/>" +
             "<input type='submit' name='SUBMIT_OK' value='OK'/></td></tr>" +
-            "</table></form></body></html>";
+            "</table></form>" +
+            "<br/><br/>" +
+            "<b>Help</b><br/>" +
+            "<ul>" +
+            "<li><b>GoodData Username</b> is your GoodData username (your e-mail). Go to the <a href='https://secure.gooddata.com'>GoodData platform main page</a> to create your account.</li>" +
+            "<li><b>Insights Graph API URL</b> is the Facebook Graph API URL, that contains the data for a specific object in format 'object_id/metric/period'. " +
+            "See the <a href='http://developers.facebook.com/docs/reference/api/insights/'>Facebook documentation</a> for more details.</li>" +
+            "<li><b>Start Date</b> is the date when you want to retrieve the Facebook metrics from.</li>" +
+            "<li><b>End Date</b> is the date when you want to retrieve the Facebook metrics till. Please note that Facebook supports at most 30 days periods.</li>" +
+            "<li><b>GoodData Project</b> is the GoodData project hash (e.g. ca6a1r1lbfwpt2v05k36nbc0cjpu7lh9). If the 'Create New GoodData Project' is checked, this is ignored.</li>" +
+            "<li><b>Create New GoodData Project</b> creates a new GoodData project. Once the project is ready, you'll receive the invitation e-mail.</li>" +
+            "</ul>" +
+            "</body></html>";
     static final String result = "<!DOCTYPE HTML SYSTEM><html><head><title>GoodData Data Synchronization Result</title></head>" +
             "<body>%MSG%</body></html>";
+
+    private static final DateTimeFormatter baseFmt = DateTimeFormat.forPattern(Constants.DEFAULT_DATE_FMT_STRING);
 
     private static Set<String> acceptedParams = new HashSet<String>();
 
 
     static {
-        String[] p = {"TOKEN", "BASE_URL", "START_DATE", "END_DATE", "EMAIL"};
+        String[] p = {"TOKEN", "BASE_URL", "START_DATE", "END_DATE", "EMAIL", "GDC_PROJECT_HASH"};
         acceptedParams.addAll(Arrays.asList(p));
     }
 
@@ -79,12 +95,18 @@ public class WebInterface extends HttpServlet {
         String token = extractToken(request);
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
+        String t = form;
+        DateTime today = new DateTime();
+        DateTime start = today.minusDays(30);
+        t = t.replace("%START_DATE_INITIAL_VALUE%", baseFmt.print(start));
+        t = t.replace("%END_DATE_INITIAL_VALUE%",baseFmt.print(today));
         if(token != null) {
-            String t = form.replace("%TOKEN%",token);
+            t = t.replace("%TOKEN%",token);
             out.print(t);
         }
         else {
-            out.print(form.replace("%TOKEN%",""));
+            t = t.replace("%TOKEN%","");
+            out.print(t);
         }
         out.close();
     }
