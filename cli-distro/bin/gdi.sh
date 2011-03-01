8273f5412d2571eb318f7673aceaf9c02e087c2f
Lubomir Rintel <lubo.rintel@gooddata.com>
2011-03-01 19:32:30 +0100

    Fix missing escaping

--- cli-distro/bin/gdi.sh
+++ cli-distro/bin/gdi.sh
@@ -2,7 +2,7 @@
 
 # java run wrapper
 
-A=`readlink $0` # resolve symlinks
+A=`readlink "$0"` # resolve symlinks
 A="${A:-$0}"      # if original wasn't a symlink, BSD returns empty string
 SCRIPT_REAL_PATH=$(cd "${A%/*}" && echo "$PWD/${A##*/}") # path to original shell script, resolved from relative paths
 PROJECT_DIR=`dirname "$SCRIPT_REAL_PATH"`
