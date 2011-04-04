947caefc1ecef9ed693adf6cd9fbda6051cd9e5d
Pavel Kolesnikov <pavel@gooddata.com>
2011-04-03 21:45:41 -0700

    1.2.21-beta - fixed date/time related issues

--- backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
+++ backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
@@ -2502,7 +2502,7 @@ public class GdcRESTApiWrapper {
     private static <T extends HttpMethod> T configureHttpMethod(T request) {
         request.setRequestHeader("Content-Type", "application/json");
         request.setRequestHeader("Accept", "application/json");
-        request.setRequestHeader("User-Agent", "GoodData CL/1.2.20-BETA");
+        request.setRequestHeader("User-Agent", "GoodData CL/1.2.21-BETA");
         return request;
     }
 
