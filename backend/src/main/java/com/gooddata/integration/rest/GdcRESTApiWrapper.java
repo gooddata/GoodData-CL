f88c53aaf5bc50b4faa00060e03d5eab0805af10
Pavel Kolesnikov <pavel@gooddata.com>
2011-05-17 23:13:46 -0700

    1.2.25-BETA

--- backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
+++ backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
@@ -2502,7 +2502,7 @@ public class GdcRESTApiWrapper {
     private static <T extends HttpMethod> T configureHttpMethod(T request) {
         request.setRequestHeader("Content-Type", "application/json");
         request.setRequestHeader("Accept", "application/json");
-        request.setRequestHeader("User-Agent", "GoodData CL/1.2.24-BETA");
+        request.setRequestHeader("User-Agent", "GoodData CL/1.2.25-BETA");
         return request;
     }
 
