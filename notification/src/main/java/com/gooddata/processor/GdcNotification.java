a2902d5a02065eba69461be6467ecf7445abac34
Pavel Kolesnikov <pavel@gooddata.com>
2011-05-17 23:13:46 -0700

    1.2.25-BETA

--- notification/src/main/java/com/gooddata/processor/GdcNotification.java
+++ notification/src/main/java/com/gooddata/processor/GdcNotification.java
@@ -386,7 +386,7 @@ public class GdcNotification {
         }
 
         if(cp.containsKey(CLI_PARAM_VERSION[0])) {
-            l.info("GoodData Notification Tool version 1.2.24-BETA" +
+            l.info("GoodData Notification Tool version 1.2.25-BETA" +
                     ((BUILD_NUMBER.length()>0) ? ", build "+BUILD_NUMBER : "."));
             System.exit(0);
 
