540b00cc2af9441235812d26d8dcc25936398052
Zdenek Svoboda <zsvoboda@gmail.com>
2012-06-07 13:56:54 +0200

    Fixed the port parameter to be available via the gdi.properties

--- backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
+++ backend/src/main/java/com/gooddata/integration/rest/GdcRESTApiWrapper.java
@@ -2932,7 +2932,7 @@ public class GdcRESTApiWrapper {
         request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
         request.setRequestHeader("Accept", "application/json");
         request.setRequestHeader("Accept-Charset", "utf-u");
-        request.setRequestHeader("User-Agent", "GoodData CL/1.2.52");
+        request.setRequestHeader("User-Agent", "GoodData CL/1.2.53");
         return request;
     }
 
