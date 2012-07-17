0936111c4c49f69d65907c9455819eb3d711acf4
Pavel Kolesnikov <pavel@gooddata.com>
2012-07-17 13:04:39 -0700

    1.2.56 - CreateUser supports email parameter

--- cli/src/main/java/com/gooddata/processor/GdcDI.java
+++ cli/src/main/java/com/gooddata/processor/GdcDI.java
@@ -378,7 +378,7 @@ public class GdcDI implements Executor {
 
         if (cp.containsKey(CLI_PARAM_VERSION[0])) {
 
-            l.info("GoodData CL version 1.2.55" +
+            l.info("GoodData CL version 1.2.56" +
                     ((BUILD_NUMBER.length() > 0) ? ", build " + BUILD_NUMBER : "."));
             System.exit(0);
 
