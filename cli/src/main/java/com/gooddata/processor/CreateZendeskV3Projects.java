86fc7ffc07f51c09a271daa6cedb60685629a63b
Pavel Kolesnikov <pavel@gooddata.com>
2013-02-13 13:42:12 -0800

    version 1.2.66

--- cli/src/main/java/com/gooddata/processor/CreateZendeskV3Projects.java
+++ cli/src/main/java/com/gooddata/processor/CreateZendeskV3Projects.java
@@ -267,7 +267,7 @@ public class CreateZendeskV3Projects {
         CliParams cp = new CliParams();
 
         if (cp.containsKey(CLI_PARAM_VERSION[0])) {
-            l.info("GoodData CL version 1.2.65");
+            l.info("GoodData CL version 1.2.66");
             System.exit(0);
         }
 
