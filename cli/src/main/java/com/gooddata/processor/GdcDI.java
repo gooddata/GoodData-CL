83604e0e98b5b7ad356a06d008f74c0a2e62bca6
Pavel Kolesnikov <pavel@gooddata.com>
2014-01-22 12:47:52 -0800

    1.2.71 - rebuildLabels actually added in this version

--- cli/src/main/java/com/gooddata/processor/GdcDI.java
+++ cli/src/main/java/com/gooddata/processor/GdcDI.java
@@ -387,7 +387,7 @@ public class GdcDI implements Executor {
 
         if (cp.containsKey(CLI_PARAM_VERSION[0])) {
 
-            l.info("GoodData CL version 1.2.70" +
+            l.info("GoodData CL version 1.2.71" +
                     ((BUILD_NUMBER.length() > 0) ? ", build " + BUILD_NUMBER : "."));
             System.exit(0);
 
