d3ecb6d66abcb1d7b549e9fb10573260be3760b7
OrDTesters <ordtesters@gmail.com>
2020-01-10 18:51:47 -0600

    Initializing DataTypeGuess so testIsDate can pass when run by itself

--- connector/src/test/java/com/gooddata/csv/DataTypeGuessTest.java
+++ connector/src/test/java/com/gooddata/csv/DataTypeGuessTest.java
@@ -53,6 +53,7 @@ public class DataTypeGuessTest extends TestCase {
     }
 
     public void testIsDate() {
+        DataTypeGuess guesser = new DataTypeGuess(true);
         assertEquals("yyyy-MM-dd", DataTypeGuess.getDateFormat("2010-11-12"));
         assertNull(DataTypeGuess.getDateFormat("2010-13-12"));
         assertEquals("MM/dd/yyyy", DataTypeGuess.getDateFormat("11/12/2010"));
